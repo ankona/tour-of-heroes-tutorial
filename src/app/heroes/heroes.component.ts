17941785f776e82224f21023c818307bb180a214
Chris McBride <christopher.mcbride@gmail.com>
2020-09-20 02:46:24 -0400

    complete `feature component` tutorial

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+// import { Pipe, PipeTransform } from '@angular/core';
+// import { CommonModule } from "@angular/common";
+
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+
+export class HeroesComponent implements OnInit {
+  heroes = HEROES;
+  selectedHero: Hero;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    // alert(hero.name);
+  }
+
+}

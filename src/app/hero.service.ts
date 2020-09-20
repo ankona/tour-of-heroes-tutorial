9de7626297fe2f93e35463e9374b3206bea7f954
Chris McBride <christopher.mcbride@gmail.com>
2020-09-20 03:19:32 -0400

    completed `add services` tutorial

---
+++ src/app/hero.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
+import { Message } from '@angular/compiler/src/i18n/i18n_ast';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor(
+    private messageService : MessageService
+  ) { }
+
+  getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
+  }
+}
